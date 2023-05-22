1ce77a861e45f55ec053e839584d8a64cafabc15
KwakYounHo <kwakyh951@gmail.com>
2023-05-22 17:51:18 +0900

    rebase 후 원격 브랜치 상태 보기

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('첫 브랜치에서 만든 첫 파일');
 console.log('두 번째 브랜치 생성하기 전 브랜치 업데이트');
 console.log('세 번째 rebase를 위한 수정');
 console.log('네 번째 강제로 푸시하기?!');
+console.log('다섯 번째 rebase 후 원격 상태 보기');
